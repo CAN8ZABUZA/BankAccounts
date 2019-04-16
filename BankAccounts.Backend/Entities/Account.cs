c153ec8f44104a06ff64d30b247cba667311ba6a
alvaro <alvaro8shark@gmail.com>
2019-04-16 20:56:48 +0200

    1000 - Creación entidades

--- BankAccounts.Backend/Entities/Account.cs
+++ BankAccounts.Backend/Entities/Account.cs
@@ -9,5 +9,67 @@ namespace BankAccounts.Backend.Entities
         public string IBAN { get; private set; }
         public float Ammount { get; private set; }
         public string Owner { get; private set; }
+        public List<Transaction> Transactions { get; private set; }
+
+        public Account(string IBAN, float Ammount, string Owner)
+            : this()
+        {
+            this.IBAN = IBAN;
+            this.Ammount = Ammount;
+            this.Owner = Owner;
+        }
+
+        private Account()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+        public Account AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+            return this;
+        }
+
+        public Account AddTransactions(List<Transaction> transactions)
+        {
+            foreach(var transaction in transactions)
+            {
+                AddTransaction(transaction);
+            }
+            return this;
+        }
+
+        public Account DeleteTransaction(Transaction transaction)
+        {
+            if (Transactions.Contains(transaction))
+            {
+                Transactions.Remove(transaction);
+            }
+            return this;
+        }
+
+        public Account DeleteTransactions(List<Transaction> transactions)
+        {
+            foreach(var transaction in transactions)
+            {
+                DeleteTransaction(transaction);
+            }
+            return this;
+        }
+
+        public Account DeleteAllTransactions()
+        {
+            foreach(var transaction in Transactions)
+            {
+                DeleteTransaction(transaction);
+            }
+            return this;
+        }
+
+        public Account ChangeOwner(string owner)
+        {
+            Owner = owner;
+            return this;
+        }
     }
 }
---
+++ BankAccounts.Backend/Entities/Transaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccounts.Backend.Entities
+{
+    public class Transaction
+    {
+    }
+}
