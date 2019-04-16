7f8c8955075461480803a331e9e6650a75f82a3f
alvaro <alvaro8shark@gmail.com>
2019-04-16 20:11:08 +0200

    Agregar archivos de proyecto.

---
+++ BankAccounts.Backend/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccounts.Backend.Entities
+{
+    public class Account
+    {
+        public string IBAN { get; private set; }
+        public float Ammount { get; private set; }
+        public string Owner { get; private set; }
+    }
+}
